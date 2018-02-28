cb7a9f42dad12996b388004029c6d38119c5e6d4
Somesh Ganesh <someshganesh94@gmail.com>
2018-02-28 18:20:15 -0500

    Cleaned vibrato files in spectrogram exercise

--- e3-spectrogram/src/inc/Vibrato.h
+++ e3-spectrogram/src/inc/Vibrato.h
@@ -16,43 +16,7 @@ class CVibrato
 {
 public:
     
-    enum VibratoParam_t
-    {
-        kParamWidth,
-        kParamModFreq,
-        kSampleRate,
-        
-        kNumVibratoParams
-    };
-    
-    CVibrato();
-    
-    ~CVibrato();
-    
-    static Error_t create (CVibrato*& pCVibrato);
-    
-    static Error_t destroy (CVibrato*& pCVibrato);
-    
-    Error_t init (float fSampleRateInHz, int iNumChannels); // put arguments here
-
-    Error_t reset();
-    
-    Error_t setParam(VibratoParam_t eParam, float fParamValue);
-    
-    float getParam(VibratoParam_t eParam) const;
-    
-    Error_t process(float **ppfInputBuffer, float **ppfOutputBuffer, int iNumberOfFrames);
-    
 private:
-    
-    bool m_bIsInitialized;
-    
-    CLfo *m_pCLfo;
-    CRingBuffer<float> *m_pCRingbuffer;
-    
-    float m_fSampleRateInHz;
-    float m_fModFreqInSamples;
-    float m_fWidthInSamples;
 
 };
 
