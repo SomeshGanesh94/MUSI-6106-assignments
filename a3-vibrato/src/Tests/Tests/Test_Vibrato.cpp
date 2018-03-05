84e4c68f174aa4ae3163ef025ce0662bbac06a71
Somesh Ganesh <someshganesh94@gmail.com>
2018-03-04 22:50:12 -0500

    Updated with validation tests

--- a3-vibrato/src/Tests/Tests/Test_Vibrato.cpp
+++ a3-vibrato/src/Tests/Tests/Test_Vibrato.cpp
@@ -1,6 +1,7 @@
 #include "MUSI6106Config.h"
 
 #ifdef WITH_TESTS
+#include <iostream>
 #include <cassert>
 #include <cstdio>
 #include <algorithm>
@@ -17,32 +18,200 @@ SUITE(Vibrato)
 {
     struct VibratoData
     {
+        // e.g., a member vibrato object to be reused in each test
+        
+        CVibrato* pCVibrato = 0;
+        float**   ppfInputSig = 0;
+        float**   ppfOutputSig = 0;
+        int iNumChannels;
+        float fSampleRateInHz;
+        float fBasicDelay;
+        float fWidth;
+        float fModInHz;
+        float fTestSigFrequency;
+        int iSigLength;
+        
         VibratoData()
         {
             // setup
             // e.g., allocate a vibrato object and test signal (newly created for each test case)
+            iNumChannels = 2;
+            fSampleRateInHz = 44100;
+            fBasicDelay = 0.1f;
+            fWidth = 0.001f;
+            fModInHz = 15;
+            iSigLength = 4096;
+            fTestSigFrequency = 1000;
+            
+            CVibrato::create(pCVibrato);
+            pCVibrato->init(fSampleRateInHz, fModInHz, fBasicDelay, iNumChannels);
+
+            ppfInputSig = new float*[iNumChannels];
+            ppfOutputSig = new float*[iNumChannels];
+            
+            for (int i = 0; i < iNumChannels; i++)
+            {
+                ppfInputSig[i] = new float[iSigLength];
+                CVectorFloat::setZero(ppfInputSig[i], iSigLength);
+                ppfOutputSig[i] = new float[iSigLength];
+                CVectorFloat::setZero(ppfOutputSig[i], iSigLength);
+            }
+            
         }
 
         ~VibratoData()
         {
             // teardown
             // e.g., deallocate the vibrato object and test signal
+            CVibrato::destroy(pCVibrato);
+            for (int i = 0; i < iNumChannels; i++) 
+            {
+                delete [] ppfInputSig[i];
+                delete [] ppfOutputSig[i];
+            }
+            delete [] ppfInputSig;
+            delete [] ppfOutputSig;
+            ppfInputSig = 0;
+            ppfOutputSig = 0;
+        }
+        
+        void reset()
+        {
+            for (int i = 0; i < iNumChannels; i++)
+            {
+                for (int j = 0; j < iSigLength; j++)
+                {
+                    ppfInputSig[i][j] = 0;
+                    ppfOutputSig[i][j] = 0;
+                }
+            }
         }
 
         // e.g., a reusable process() function
-
-        // e.g., a member vibrato object to be reused in each test
+        
+        void process()
+        {
+            pCVibrato->process(ppfInputSig, ppfOutputSig, iSigLength);
+        }
+        
+        
     };
+    
+    
+    
 
-    TEST(MyTestWithNoFixture)
+//    TEST(MyTestWithNoFixture)
+//    {
+//        // e.g., allocate & deallocate local data for testing
+//    }
+
+    TEST_FIXTURE(VibratoData, ModAmpEqaulsZero)
     {
-        // e.g., allocate & deallocate local data for testing
+        float fModAmp = 0;
+        reset();
+        pCVibrato->reset();
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            CSynthesis::generateSine(ppfInputSig[i], fTestSigFrequency, fSampleRateInHz, iSigLength);
+        }
+        pCVibrato->init(fSampleRateInHz, fModInHz, fModAmp, iNumChannels);
+        process();
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            for (int j = 0; j < (iSigLength - static_cast<int>(round(fWidth*fSampleRateInHz))); j++)
+            {
+                CHECK_CLOSE(ppfInputSig[i][j], ppfOutputSig[i][j + static_cast<int>(round(fWidth*fSampleRateInHz))], 1e-3);
+            }
+        }
+    }
+    
+    TEST_FIXTURE(VibratoData, ZeroInput)
+    {
+        reset();
+        pCVibrato->reset();
+        pCVibrato->init(fSampleRateInHz, fModInHz, fWidth, iNumChannels);
+        process();
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            for (int j = 0; j < iSigLength; j++)
+            {
+                CHECK_EQUAL(0, ppfOutputSig[i][j]);
+            }
+        }
+    }
+    
+    TEST_FIXTURE(VibratoData, DCInput)
+    {
+        reset();
+        pCVibrato->reset();
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            CSynthesis::generateDc(ppfInputSig[i], iSigLength);
+        }
+        pCVibrato->init(fSampleRateInHz, fModInHz, fWidth, iNumChannels);
+        process();
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            for (int j = 0; j < (iSigLength - static_cast<int>(round(2*fWidth*fSampleRateInHz))); j++)
+            {
+                CHECK_CLOSE(ppfInputSig[i][j], ppfOutputSig[i][j + static_cast<int>(round(2*fWidth*fSampleRateInHz))], 1e-3);
+            }
+        }
+    }
+    
+//    TEST_FIXTURE(VibratoData, ModFreqEqaulsZero)
+//    {
+//        float fModFreq = 0;
+//        reset();
+//        pCVibrato->reset();
+//        for (int i = 0; i < iNumChannels; i++)
+//        {
+//            CSynthesis::generateSine(ppfInputSig[i], fTestSigFrequency, fSampleRateInHz, iSigLength);
+//        }
+//        pCVibrato->init(fSampleRateInHz, fModFreq, fWidth, iNumChannels);
+//        process();
+//        for (int i = 0; i < iNumChannels; i++)
+//        {
+//            for (int j = 0; j < (iSigLength - static_cast<int>(round(fWidth*fSampleRateInHz))); j++)
+//            {
+//                CHECK_CLOSE(ppfInputSig[i][j], ppfOutputSig[i][j + static_cast<int>(round(fWidth*fSampleRateInHz))], 1e-3);
+//            }
+//        }
+//    }
+    
+    TEST_FIXTURE(VibratoData, VaryingInputBlockSize)
+    {
+        int iBlockLength = 512;
+        float**   ppfTempOutputSig = 0;
+        ppfTempOutputSig = new float*[iNumChannels];
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            ppfTempOutputSig[i] = new float[iSigLength];
+            CVectorFloat::setZero(ppfTempOutputSig[i], iSigLength);
+        }
+        reset();
+        pCVibrato->reset();
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            CSynthesis::generateSine(ppfInputSig[i], fTestSigFrequency, fSampleRateInHz, iSigLength);
+        }
+        pCVibrato->init(fSampleRateInHz, fModInHz, fWidth, iNumChannels);
+        process();
+        pCVibrato->process(ppfInputSig, ppfTempOutputSig, iBlockLength);
+        for (int i = 0; i < iNumChannels; i++)
+        {
+            for (int j = 0; j < iSigLength; j++)
+            {
+                CHECK_CLOSE(ppfOutputSig[i][j], ppfTempOutputSig[i][j], 1e-3);
+            }
+        }
     }
 
-    TEST_FIXTURE(VibratoData, MyTestWithFixture)
-    {
-        // e.g., you can use the "VibratoData" contents
-    }
+    
+    
+    
+    
 }
+
 
 #endif //WITH_TESTS
