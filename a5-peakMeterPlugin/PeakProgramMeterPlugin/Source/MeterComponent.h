5624dfad12e2d3ff1c433bb11d9852fb8062143c
Somesh Ganesh <someshganesh94@gmail.com>
2018-04-17 19:24:28 -0400

    First commit for ppm plugin

---
+++ a5-peakMeterPlugin/PeakProgramMeterPlugin/Source/MeterComponent.h
@@ -0,0 +1,61 @@
+/*
+  ==============================================================================
+
+    MeterComponent.h
+    Created: 17 Apr 2018 7:06:45pm
+    Author:  Somesh Ganesh
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/*
+*/
+class MeterComponent    : public Component
+{
+public:
+    MeterComponent()
+    {
+        // In your constructor, you should add any child components, and
+        // initialise any special settings that your component needs.
+
+    }
+
+    ~MeterComponent()
+    {
+    }
+
+    void paint (Graphics& g) override
+    {
+        /* This demo code just fills the component's background and
+           draws some placeholder text to get you started.
+
+           You should replace everything in this method with your own
+           drawing code..
+        */
+
+        g.fillAll (Colours::black);   // clear the background
+
+        g.setColour (Colours::grey);
+        g.drawRect (getLocalBounds(), 1);   // draw an outline around the component
+
+        g.setColour (Colours::white);
+        g.setFont (14.0f);
+        g.drawText ("MeterComponent", getLocalBounds(),
+                    Justification::centred, true);   // draw some placeholder text
+    }
+
+    void resized() override
+    {
+        // This method is where you should set the bounds of any child
+        // components that your component contains..
+
+    }
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MeterComponent)
+};
